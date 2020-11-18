819ea92f87fcb95eef4bd621e8d62cd1cfec1546
James Rogers <jrogers.dev@gmail.com>
2020-11-18 22:44:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ document.addEventListener(
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
+        console.log(data.message);
       })
     ;
   }
