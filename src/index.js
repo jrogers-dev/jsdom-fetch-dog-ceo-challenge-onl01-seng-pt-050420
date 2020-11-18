2d41a5d5726d83c9ac124eaf265dc43b4e28d20f
James Rogers <jrogers.dev@gmail.com>
2020-11-18 23:19:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ document.addEventListener(
             "click",
             function(e) {
             
-            self.fontcolor("red");
+            breed.fontcolor("red");
           });
         }
       })
