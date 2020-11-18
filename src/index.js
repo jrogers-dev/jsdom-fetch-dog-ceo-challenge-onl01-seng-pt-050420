28e5dc30b368f09576441cb3c2334117c14ec15e
James Rogers <jrogers.dev@gmail.com>
2020-11-18 22:38:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log('%c HI', 'color: firebrick');
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
 document.addEventListener(
   "DOMContentLoaded", 
